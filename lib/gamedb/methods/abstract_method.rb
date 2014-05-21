52e0e60da62adc42ac6269852ba6e9dd18c913eb
bcassell <bcassell@umich.edu>
2014-05-21 12:38:36 -0400

    ensure that caches are clearing

--- lib/gamedb/methods/abstract_method.rb
+++ lib/gamedb/methods/abstract_method.rb
@@ -17,7 +17,9 @@ module AbstractMethod
   def uncache
     DB.disconnect
     system('echo $EXEC | sudo -u postgres /usr/local/pgsql/bin/pg_ctl stop -D /usr/local/pgsql/data')
+    sleep 1
     system('echo $EXEC | sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"')
+    sleep 1
     system('echo $EXEC | sudo -u postgres /usr/local/pgsql/bin/pg_ctl start -D /usr/local/pgsql/data -o "--config-file=/home/bcassell/DDGT/db_configs/maxing_out.conf"')
     sleep 1
   end
